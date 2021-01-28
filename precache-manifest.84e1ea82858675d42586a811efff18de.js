bf2fc2b3eb8e848b9716c72abc89405e111b9ea6
Aditya Kumar Jha <mail@adijha.com>
2021-01-28 07:38:08 +0530

    Updates

---
+++ precache-manifest.84e1ea82858675d42586a811efff18de.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7213dae5d912222468ee71bd99959d7",
+    "url": "/rss-news-feed/index.html"
+  },
+  {
+    "revision": "38e058c26957690cb8ce",
+    "url": "/rss-news-feed/static/css/2.87a9c7a2.chunk.css"
+  },
+  {
+    "revision": "69c2773f8b29f9fc2e01",
+    "url": "/rss-news-feed/static/css/main.c12055e4.chunk.css"
+  },
+  {
+    "revision": "38e058c26957690cb8ce",
+    "url": "/rss-news-feed/static/js/2.21dd2793.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/rss-news-feed/static/js/2.21dd2793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c2773f8b29f9fc2e01",
+    "url": "/rss-news-feed/static/js/main.5dc7110a.chunk.js"
+  },
+  {
+    "revision": "01103664bc100b7a4e71",
+    "url": "/rss-news-feed/static/js/runtime-main.8e1d7d38.js"
+  }
+]);
